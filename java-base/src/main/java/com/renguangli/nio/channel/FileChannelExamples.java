8c9dc88c2a08e9a97ad6750306c22ec548a87238
renguangli <renguangli@bonc.com.cn>
2018-09-03 20:04:52 +0800

    feature:nio读FileChannel内容

--- java-base/src/main/java/com/renguangli/nio/channel/FileChannelExamples.java
+++ java-base/src/main/java/com/renguangli/nio/channel/FileChannelExamples.java
@@ -1,14 +1,9 @@
 package com.renguangli.nio.channel;
 
-import sun.misc.BASE64Encoder;
-
-import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 /**
  * FileChannelExamples
@@ -20,24 +15,21 @@ public class FileChannelExamples {
 
     public static void main(String[] args) throws IOException {
         StringBuilder sb = new StringBuilder();
-        RandomAccessFile file = new RandomAccessFile("D:/data/nio-data.txt", "rw");
+        RandomAccessFile file = new RandomAccessFile("D:a.txt", "rw");
         FileChannel fileChannel = file.getChannel();
-        ByteBuffer byteBuffer = ByteBuffer.allocate(64);
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
         while (fileChannel.read(byteBuffer) != -1) {
             //切换为读状态,默认写状态
             byteBuffer.flip();
             while (byteBuffer.hasRemaining()) {
-                sb.append((char)byteBuffer.getChar());
+                System.out.println("--------------");
+                sb.append(byteBuffer.getChar());
             }
             byteBuffer.clear();
         }
         file.close();
+        fileChannel.close();
         System.out.println(sb);
 
-        Path path = Paths.get("d:/data");
-        System.out.println(path.getFileName());
-        System.out.println(path.getFileSystem());
-        System.out.println(path.getNameCount());
-        System.out.println(path.getRoot());
     }
 }
